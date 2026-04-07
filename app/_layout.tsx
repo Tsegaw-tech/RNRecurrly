52d1879a81d3289a42f8120a4d1d0b0b0d910d71
unknown <tsegaw.grace@gmail.com>
2026-04-07 13:03:11 -0700

    feat: project setup,nativwind, routing, tab navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from "expo-router";
 import "../global.css";
 
-export default function RootLayout() {
+export default function RootLayoutContent() {
    return <Stack screenOptions={{headerShown:false}}/>;
 }
